07acb65655c9594b893b9197e2fbeb5b10cff369
mchoffn <32065735+mchoffn@users.noreply.github.com>
2018-05-28 12:04:17 +0200

    Add files via upload

---
+++ ny_tobias/animation_lol_ui.js
@@ -0,0 +1,440 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+(lib._33087654_1172796552862711_2815291272288272384_n = function() {
+	this.initialize(img._33087654_1172796552862711_2815291272288272384_n);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1920,1080);
+
+
+(lib.btn_prev3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.text = new cjs.Text("Forrige", "31px 'Microsoft New Tai Lue'", "#0596AA");
+	this.text.lineHeight = 43;
+	this.text.lineWidth = 100;
+	this.text.parent = this;
+	this.text.setTransform(-50,-20.6);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().ls(["#0596AA","#048AA2","#016A8D","#005A82","#0596AA","#0594A8","#048BA3","#037D9A","#016A8C","#005A82","#0596AA"],[0,0.071,0.204,0.259,0.502,0.596,0.651,0.694,0.733,0.757,1],-162.5,0,162.5,0).ss(2).p("ATqJYMgnTAAAQiUAAhphpQhohoAAiUIAAnlQAAiUBohpQBphoCUAAMAnTAAAQCUAABoBoQBpBpAACUIAAHlQAACUhpBoQhoBpiUAAg");
+	this.shape.setTransform(0,0,0.568,0.568);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#1E2328").s().p("AzpJYQiTAAhphpQhphoAAiUIAAnkQAAiUBphpQBphpCTAAMAnSAAAQCUAABpBpQBpBpAACUIAAHkQAACUhpBoQhpBpiUAAg");
+	this.shape_1.setTransform(0,0,0.568,0.568);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#0596AA"}}]}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#FFFFFF"}}]},1).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#FFFFFF"}}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-92.7,-35.1,185.5,70.2);
+
+
+(lib.btn_forfra = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.text = new cjs.Text("Forfra", "31px 'Microsoft New Tai Lue'", "#0596AA");
+	this.text.lineHeight = 43;
+	this.text.parent = this;
+	this.text.setTransform(-40.2,-20.3);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().ls(["#0596AA","#048AA2","#016A8D","#005A82","#0596AA","#0594A8","#048BA3","#037D9A","#016A8C","#005A82","#0596AA"],[0,0.071,0.204,0.259,0.502,0.596,0.651,0.694,0.733,0.757,1],-162.5,0,162.5,0).ss(2).p("ATqJYMgnTAAAQiUAAhphpQhohoAAiUIAAnlQAAiUBohpQBphoCUAAMAnTAAAQCUAABoBoQBpBpAACUIAAHlQAACUhpBoQhoBpiUAAg");
+	this.shape.setTransform(0,0,0.568,0.568);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#1E2328").s().p("AzpJYQiTAAhphpQhphoAAiUIAAnkQAAiUBphpQBphpCTAAMAnSAAAQCUAABpBpQBpBpAACUIAAHkQAACUhpBoQhpBpiUAAg");
+	this.shape_1.setTransform(0,0,0.568,0.568);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-92.7,-35.1,185.4,70.2);
+
+
+(lib.bnt_next = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.text = new cjs.Text("Næste", "31px 'Microsoft New Tai Lue'", "#0596AA");
+	this.text.lineHeight = 43;
+	this.text.lineWidth = 100;
+	this.text.parent = this;
+	this.text.setTransform(-50,-21.1);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().ls(["#0596AA","#048AA2","#016A8D","#005A82","#0596AA","#0594A8","#048BA3","#037D9A","#016A8C","#005A82","#0596AA"],[0,0.071,0.204,0.259,0.502,0.596,0.651,0.694,0.733,0.757,1],-162.5,0,162.5,0).ss(2).p("ATqJYMgnTAAAQiUAAhphpQhohoAAiUIAAnlQAAiUBohpQBphoCUAAMAnTAAAQCUAABoBoQBpBpAACUIAAHlQAACUhpBoQhoBpiUAAg");
+	this.shape.setTransform(0,0,0.568,0.568);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#1E2328").s().p("AzpJYQiTAAhphpQhphoAAiUIAAnkQAAiUBphpQBphpCTAAMAnSAAAQCUAABpBpQBpBpAACUIAAHkQAACUhpBoQhpBpiUAAg");
+	this.shape_1.setTransform(0,0,0.568,0.568);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#0596AA"}}]}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#FFFFFF"}}]},1).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{color:"#FFFFFF"}}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-92.7,-35.1,185.5,70.2);
+
+
+// stage content:
+(lib.animation_lol_ui = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_0 = function() {
+		this.stop();
+		
+		this.btn2.addEventListener("click", fl_ClickToGoToAndStopAtFrame.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame()
+		{
+			this.gotoAndStop(1);
+		}
+	}
+	this.frame_1 = function() {
+		this.btn3.addEventListener("click", fl_ClickToGoToAndStopAtFrame_7.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_7()
+		{
+			this.gotoAndStop(0);
+		}
+		
+		
+		
+		
+		this.btn4.addEventListener("click", fl_ClickToGoToAndStopAtFrame_8.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_8()
+		{
+			this.gotoAndStop(2);
+		}
+	}
+	this.frame_2 = function() {
+		this.btn5.addEventListener("click", fl_ClickToGoToAndStopAtFrame_9.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_9()
+		{
+			this.gotoAndStop(1);
+		}
+		
+		
+		
+		this.btn6.addEventListener("click", fl_ClickToGoToAndStopAtFrame_10.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_10()
+		{
+			this.gotoAndStop(3);
+		}
+	}
+	this.frame_3 = function() {
+		this.btn5.addEventListener("click", fl_ClickToGoToAndStopAtFrame_11.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_11()
+		{
+			this.gotoAndStop(2);
+		}
+		
+		
+		
+		this.btn6.addEventListener("click", fl_ClickToGoToAndStopAtFrame_12.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_12()
+		{
+			this.gotoAndStop(4);
+		}
+	}
+	this.frame_4 = function() {
+		this.btn5.addEventListener("click", fl_ClickToGoToAndStopAtFrame_13.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_13()
+		{
+			this.gotoAndStop(3);
+		}
+		
+		
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.btn_forfra.addEventListener("click", fl_ClickToGoToAndStopAtFrame_17.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_17()
+		{
+			this.gotoAndStop(0);
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1).call(this.frame_1).wait(1).call(this.frame_2).wait(1).call(this.frame_3).wait(1).call(this.frame_4).wait(1));
+
+	// Button
+	this.btn2 = new lib.bnt_next();
+	this.btn2.name = "btn2";
+	this.btn2.parent = this;
+	this.btn2.setTransform(720.5,584.2);
+	new cjs.ButtonHelper(this.btn2, 0, 1, 2);
+
+	this.btn4 = new lib.bnt_next();
+	this.btn4.name = "btn4";
+	this.btn4.parent = this;
+	this.btn4.setTransform(720.5,584.2);
+	new cjs.ButtonHelper(this.btn4, 0, 1, 2);
+
+	this.btn3 = new lib.btn_prev3();
+	this.btn3.name = "btn3";
+	this.btn3.parent = this;
+	this.btn3.setTransform(468.3,584.2);
+	new cjs.ButtonHelper(this.btn3, 0, 1, 2);
+
+	this.btn6 = new lib.bnt_next();
+	this.btn6.name = "btn6";
+	this.btn6.parent = this;
+	this.btn6.setTransform(720.5,584.2);
+	new cjs.ButtonHelper(this.btn6, 0, 1, 2);
+
+	this.btn5 = new lib.btn_prev3();
+	this.btn5.name = "btn5";
+	this.btn5.parent = this;
+	this.btn5.setTransform(468.3,584.2);
+	new cjs.ButtonHelper(this.btn5, 0, 1, 2);
+
+	this.btn_forfra = new lib.btn_forfra();
+	this.btn_forfra.name = "btn_forfra";
+	this.btn_forfra.parent = this;
+	this.btn_forfra.setTransform(720.5,584.2);
+	new cjs.ButtonHelper(this.btn_forfra, 0, 1, 1);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.btn2}]}).to({state:[{t:this.btn3},{t:this.btn4}]},1).to({state:[{t:this.btn5},{t:this.btn6}]},1).to({state:[{t:this.btn5},{t:this.btn6}]},1).to({state:[{t:this.btn5},{t:this.btn_forfra}]},1).wait(1));
+
+	// FrmNum
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#FF0000").ss(2).p("ABnBKIjNiT");
+	this.shape.setTransform(360.5,653.2);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f().s("#FF0000").ss(2).p("AnkgEIPJAK");
+	this.shape_1.setTransform(420.4,660.4);
+
+	this.shape_2 = new cjs.Shape();
+	this.shape_2.graphics.f().s("#FF0000").ss(2).p("AAAFUIAAqn");
+	this.shape_2.setTransform(371.7,692.4);
+
+	this.shape_3 = new cjs.Shape();
+	this.shape_3.graphics.f().s("#FF0000").ss(2).p("AHiAFIvDgJ");
+	this.shape_3.setTransform(418.5,726.1);
+
+	this.shape_4 = new cjs.Shape();
+	this.shape_4.graphics.f().s("#FF0000").ss(2).p("AAClKIgDKV");
+	this.shape_4.setTransform(467.9,694.3);
+
+	this.text = new cjs.Text("Her finder du en lille oversigt over din champions stats. Movement speed, ability power, attack damage, attack speed etc.", "14px 'Microsoft New Tai Lue'", "#FFFFFF");
+	this.text.lineHeight = 20;
+	this.text.lineWidth = 202;
+	this.text.parent = this;
+	this.text.setTransform(270.2,564.9);
+
+	this.instance = new lib._33087654_1172796552862711_2815291272288272384_n();
+	this.instance.parent = this;
+	this.instance.setTransform(0,0,0.673,0.673);
+
+	this.shape_5 = new cjs.Shape();
+	this.shape_5.graphics.f().s("#FF0000").ss(2).p("AAIH7IgPv1");
+	this.shape_5.setTransform(596.2,575);
+
+	this.shape_6 = new cjs.Shape();
+	this.shape_6.graphics.f().s("#FF0000").ss(2).p("AAADZIABmx");
+	this.shape_6.setTransform(517.8,677,1,1.089);
+
+	this.shape_7 = new cjs.Shape();
+	this.shape_7.graphics.f().s("#FF0000").ss(2).p("AqyACIVlgD");
+	this.shape_7.setTransform(590,700,1.031,1);
+
+	this.shape_8 = new cjs.Shape();
+	this.shape_8.graphics.f().s("#FF0000").ss(2).p("AgBl/IADL/");
+	this.shape_8.setTransform(661,664.3);
+
+	this.shape_9 = new cjs.Shape();
+	this.shape_9.graphics.f().s("#FF0000").ss(2).p("AovgBIRfAD");
+	this.shape_9.setTransform(601.7,625.7,1.056,1);
+
+	this.shape_10 = new cjs.Shape();
+	this.shape_10.graphics.f().s("#FF0000").ss(2).p("AAACcIAAk3");
+	this.shape_10.setTransform(542.4,638.9);
+
+	this.shape_11 = new cjs.Shape();
+	this.shape_11.graphics.f().s("#FF0000").ss(2).p("Aj6gBIH1AD");
+	this.shape_11.setTransform(529.8,653.4,0.508,1);
+
+	this.shape_12 = new cjs.Shape();
+	this.shape_12.graphics.f().s("#FF0000").ss(2).p("AtcAAIa5AA");
+	this.shape_12.setTransform(775.9,633.2);
+
+	this.shape_13 = new cjs.Shape();
+	this.shape_13.graphics.f().s("#FF0000").ss(2).p("AgCCkIAFlH");
+	this.shape_13.setTransform(689.5,649.1);
+
+	this.shape_14 = new cjs.Shape();
+	this.shape_14.graphics.f().s("#FF0000").ss(2).p("AAEiUIgHEp");
+	this.shape_14.setTransform(662.2,678.9);
+
+	this.shape_15 = new cjs.Shape();
+	this.shape_15.graphics.f().s("#FF0000").ss(2).p("AD+AEIn7gG");
+	this.shape_15.setTransform(687.1,693.3);
+
+	this.shape_16 = new cjs.Shape();
+	this.shape_16.graphics.f().s("#FF0000").ss(2).p("AAAiUIAAEp");
+	this.shape_16.setTransform(712.6,679.6);
+
+	this.shape_17 = new cjs.Shape();
+	this.shape_17.graphics.f().s("#FF0000").ss(2).p("Aj1ABIHrgB");
+	this.shape_17.setTransform(688.1,664.6);
+
+	this.shape_18 = new cjs.Shape();
+	this.shape_18.graphics.f().s("#FF0000").ss(2).p("AAHDRIgMmh");
+	this.shape_18.setTransform(1114.5,514.8);
+
+	this.shape_19 = new cjs.Shape();
+	this.shape_19.graphics.f().s("#FF0000").ss(2).p("AmnipINPFT");
+	this.shape_19.setTransform(1022.5,685.1);
+
+	this.text_1 = new cjs.Text("De 2 små ikoner nederst er indstillinger for henholdsvis kamera- og generelle indstillinger.", "bold 14px 'Microsoft New Tai Lue'", "#FFFFFF");
+	this.text_1.lineHeight = 20;
+	this.text_1.lineWidth = 211;
+	this.text_1.parent = this;
+	this.text_1.setTransform(894.6,605.7);
+
+	this.shape_20 = new cjs.Shape();
+	this.shape_20.graphics.f().s("#FF0000").ss(2).p("AACh3IgDDv");
+	this.shape_20.setTransform(1065.3,714);
+
+	this.shape_21 = new cjs.Shape();
+	this.shape_21.graphics.f().s("#FF0000").ss(2).p("AC0AEIlngH");
+	this.shape_21.setTransform(1082.1,703.4);
+
+	this.shape_22 = new cjs.Shape();
+	this.shape_22.graphics.f().s("#FF0000").ss(2).p("AAAtOIAAad");
+	this.shape_22.setTransform(1100.2,619.7);
+
+	this.shape_23 = new cjs.Shape();
+	this.shape_23.graphics.f().s("#FF0000").ss(2).p("AOzAFI9lgJ");
+	this.shape_23.setTransform(1193.9,535.2);
+
+	this.shape_24 = new cjs.Shape();
+	this.shape_24.graphics.f().s("#FF0000").ss(2).p("AAAuqIAAdV");
+	this.shape_24.setTransform(1288.6,629.2);
+
+	this.shape_25 = new cjs.Shape();
+	this.shape_25.graphics.f().s("#FF0000").ss(2).p("AxpANMAjSgAZ");
+	this.shape_25.setTransform(1175.7,723.3);
+
+	this.shape_26 = new cjs.Shape();
+	this.shape_26.graphics.f().s("#FF0000").ss(2).p("ABjk4IjFJy");
+	this.shape_26.setTransform(1021.5,69.6);
+
+	this.shape_27 = new cjs.Shape();
+	this.shape_27.graphics.f().s("#FF0000").ss(2).p("AU4ACMgpugAD");
+	this.shape_27.setTransform(1158.4,2.4);
+
+	this.shape_28 = new cjs.Shape();
+	this.shape_28.graphics.f().s("#FF0000").ss(2).p("AAAC0IAAln");
+	this.shape_28.setTransform(1291.5,19.7);
+
+	this.shape_29 = new cjs.Shape();
+	this.shape_29.graphics.f().s("#FF0000").ss(2).p("A00gFMAppAAL");
+	this.shape_29.setTransform(1159.8,37.9);
+
+	this.shape_30 = new cjs.Shape();
+	this.shape_30.graphics.f().s("#FF0000").ss(2).p("AAAitIAAFb");
+	this.shape_30.setTransform(1026.1,19.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance},{t:this.text,p:{x:270.2,y:564.9,text:"Her finder du en lille oversigt over din champions stats. Movement speed, ability power, attack damage, attack speed etc.",lineWidth:202}},{t:this.shape_4},{t:this.shape_3},{t:this.shape_2},{t:this.shape_1},{t:this.shape}]}).to({state:[{t:this.instance},{t:this.shape_11},{t:this.shape_10},{t:this.shape_9},{t:this.shape_8},{t:this.shape_7},{t:this.shape_6},{t:this.shape_5},{t:this.text,p:{x:512.4,y:425.8,text:"Den øverste menubar, dukker frem når du stiger i levels. Så kan du vælge en ny evne. Yderst til venstre ses din passive-evne. Dette kaldes din \"skill-bar\".",lineWidth:211}}]},1).to({state:[{t:this.instance},{t:this.shape_17},{t:this.shape_16},{t:this.shape_15},{t:this.shape_14},{t:this.shape_13},{t:this.shape_12},{t:this.text,p:{x:818.2,y:593.4,text:"Her vises dine 2 summoner spells: Ignite og Flash. ",lineWidth:211}}]},1).to({state:[{t:this.instance},{t:this.shape_25},{t:this.shape_24},{t:this.shape_23},{t:this.shape_22},{t:this.shape_21},{t:this.shape_20},{t:this.text_1},{t:this.shape_19},{t:this.shape_18},{t:this.text,p:{x:984.2,y:335.9,text:"Dette er dit minimap. Det er et kort over hele banen, så du konstant har overblik over hvor dine medspillere befinder sig, samt hvor du selv er henne. Det er vigtigt at holde øje med minimap ofte , så du også kan følge med i hvad dine modstandere laver.",lineWidth:230}}]},1).to({state:[{t:this.instance},{t:this.shape_30},{t:this.shape_29},{t:this.shape_28},{t:this.shape_27},{t:this.shape_26},{t:this.text,p:{x:951.5,y:102.9,text:"I dette hjørne finder du en lille oversigt over hvor mange kills dit eget hold(blå) har og hvor mange kills modstander holdet(rød) har. Her ses også dine egne kills, deaths og assists. Hvor mange minons du har dræbt, samt et ur, der viser hvor længe det pågælende spil har været i gang.\n\nHer er 1 godt tip:\nBaron Nashor spawner efter 20 minutter !!",lineWidth:280}}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(646.5,363.5,1292.7,821.7);
+// library properties:
+lib.properties = {
+	id: '1AF9AE17DF669B468B01704118703311',
+	width: 1293,
+	height: 727,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/_33087654_1172796552862711_2815291272288272384_n.png", id:"_33087654_1172796552862711_2815291272288272384_n"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['1AF9AE17DF669B468B01704118703311'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
